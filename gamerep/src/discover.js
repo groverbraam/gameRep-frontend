bc110b30294ddbcea145c46f668b5b6dbbeb7224
Gover Braam <groverbraam@gmail.com>
2022-03-04 15:11:54 -0500

    ?

--- gamerep/src/discover.js
+++ gamerep/src/discover.js
@@ -1,7 +1,18 @@
-import React from 'react';
-const Discover = () => {
-  <>
+import React, {useState, useEffect} from 'react';
+const Discover = (props) => {
 
-  </>
+  return(
+    <>
+    {props.games ?
+      props.games.map((game) => {
+        return <div> {
+          game.title
+        }
+        </div>
+      })
+    :
+    null }
+    </>
+  )
 }
 export default Discover;
